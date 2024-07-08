05bc171e43f9158d4a074442746b82ab10dd64a2
its_achhyob <achhyob100@gmail.com>
2024-07-09 01:23:09 +0545

    first commit

---
+++ app/src/main/java/com/example/midtermproject/WelcomeActivity.java
@@ -0,0 +1,49 @@
+package com.example.midtermproject;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    RelativeLayout welcomeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        welcomeLayout = findViewById(R.id.Welcome);
+        Intent myintent = getIntent();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater my = getMenuInflater();
+        my.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+            return super.onOptionsItemSelected(item);
+
+
+    }
+}
+
+
